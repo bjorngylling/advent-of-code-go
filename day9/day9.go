09fea6bb707f29c46d78ceda827d32ab983afe3f
Björn Gylling <bjorn.gylling@gmail.com>
2018-07-20 22:17:59 +0200

    Add day 9 solution

--- day9/day9.go
+++ day9/day9.go
@@ -1,10 +1,55 @@
 package day9
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+	"io"
+	"bufio"
+)
 
 func Answer() {
+	file, err := os.Open("./day9/day9_input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-
+	scanner := bufio.NewScanner(io.Reader(file))
+	scanner.Scan()
+	input := scanner.Text()
+	groupScore, garbageCount := process(input)
 	fmt.Println("-- Day 9 --")
-	fmt.Printf("\n\n")
+	fmt.Printf("group_score=%d,garbage_count=%d\n\n", groupScore, garbageCount)
+}
+
+func process(input string) (int, int) {
+	score, garbageCount, depth := 0, 0, 1
+	skip, inGarbage := false, false
+	for _, r := range input {
+		if skip {
+			skip = false
+			continue
+		}
+		if !inGarbage {
+			switch r {
+			case '{':
+				score += depth
+				depth++
+			case '}':
+				depth--
+			case '<':
+				inGarbage = true
+			}
+		} else if r == '>' {
+			inGarbage = false
+		} else if r != '!' {
+			garbageCount++
+		}
+
+		if r == '!' {
+			skip = true
+		}
+	}
+	return score, garbageCount
 }
---
+++ day9/day9_test.go
@@ -0,0 +1,50 @@
+package day9
+
+import (
+"testing"
+)
+
+func assertEqual(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Fatalf("%s != %s", a, b)
+	}
+}
+
+func TestBasicGroups(t *testing.T) {
+	n, _ := process("{}")
+	assertEqual(t, 1, n)
+	n, _ = process("{{}}")
+	assertEqual(t, 3, n)
+	n, _ = process("{{{}}}")
+	assertEqual(t, 6, n)
+	n, _ = process("{{},{}}")
+	assertEqual(t, 5, n)
+	n, _ = process("{{{},{},{{}}}}")
+	assertEqual(t, 16, n)
+}
+func TestGroupsWithGarbage(t *testing.T) {
+	n, _ := process("{<a>,<a>,<a>,<a>}")
+	assertEqual(t, 1, n)
+	n, _ = process("{{<ab>},{<ab>},{<ab>},{<ab>}}")
+	assertEqual(t, 9, n)
+	n, _ = process("{{<ab>},{<{}ab>},{<}ab>},{<ab>}}")
+	assertEqual(t, 9, n)
+}
+func TestGroupsWithEscapeChar(t *testing.T) {
+	n, _ := process("{{<!!>},{<!!>},{<!!>},{<!!>}}")
+	assertEqual(t, 9, n)
+	n, _ = process("{{}}!{}")
+	assertEqual(t, 3, n)
+	n, _ = process("{{<a!>},{<a!>},{<a!>},{<ab>}}")
+	assertEqual(t, 3, n)
+}
+func TestGarbageCount(t *testing.T) {
+	_, n := process("{{}}!{}")
+	assertEqual(t, 0, n)
+	_, n = process("<{!>}>")
+	assertEqual(t, 2, n)
+	_, n = process("{{<!!>},{<!!>},{<!!>},{<!!>}}")
+	assertEqual(t, 0, n)
+	_, n = process("{{<a!>},{<a!>},{<a!>},{<ab>}}")
+	assertEqual(t, 17, n)
+}
