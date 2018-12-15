2f04f492bb64397010e3f92a0c8596d154b82d7a
Björn Gylling <bjorn.gylling@gmail.com>
2018-12-15 15:28:43 +0100

    wip for day 13

---
+++ 13_mine_cart_madness/day13.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+type Position image.Point
+type Speed image.Point
+
+var North = Speed{Y: -1}
+var East = Speed{X: 1}
+var South = Speed{Y: 1}
+var West = Speed{X: -1}
+
+type Cart struct {
+	pos   Position
+	speed Speed
+}
+
+func Pos(x, y int) Position {
+	return Position{X: x, Y: y}
+}
+
+func parseInput(input string) ([]string, []Cart) {
+	lines := strings.Split(input, "\n")
+	var carts []Cart
+	for y, ln := range lines {
+		for x, c := range ln {
+			switch c {
+			case rune('^'):
+				carts = append(carts, Cart{Pos(x, y), North})
+			case rune('>'):
+				carts = append(carts, Cart{Pos(x, y), East})
+			case rune('V'):
+				carts = append(carts, Cart{Pos(x, y), South})
+			case rune('<'):
+				carts = append(carts, Cart{Pos(x, y), West})
+			}
+		}
+	}
+	return lines, carts
+}
+
+func main() {
+	fileContent, err := ioutil.ReadFile("13_mine_cart_madness/day13_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Day 13 part 1 result: %+v\n", fileContent)
+
+	fmt.Printf("Day 13 part 2 result: %+v\n", nil)
+}
---
+++ 13_mine_cart_madness/day13_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var input = `/->-\        
+|   |  /----\
+| /-+--+-\  |
+| | |  | v  |
+\-+-/  \-+--/
+  \------/   `
+
+func TestParseInput(t *testing.T) {
+	grid, carts := parseInput(input)
+	eGrid := []string{
+		`/---\        `,
+		`|   |  /----\`,
+		`| /-+--+-\  |`,
+		`| | |  | |  |`,
+		`\-+-/  \-+--/`,
+		`  \------/   `}
+	eCarts := []Cart{{Pos(2, 0), East}}
+
+	if !reflect.DeepEqual(grid, eGrid) {
+		t.Errorf("Expected the grid to be %+v but was %+v", eGrid, grid)
+	}
+	if !reflect.DeepEqual(carts, eCarts) {
+		t.Errorf("Expected the mine carts be %+v but was %+v", eCarts, carts)
+	}
+}
