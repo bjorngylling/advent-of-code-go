aa1dea65ec58ebfdef8a13da80849c7d992dee7b
Björn Gylling <bjorn.gylling@gmail.com>
2018-12-16 18:30:55 +0100

    add skeleton for day 14

---
+++ 14_chocolate_charts/day14.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Day 14 part 1 result: %+v\n", nil)
+
+	fmt.Printf("Day 14 part 2 result: %+v\n", nil)
+}
---
+++ 14_chocolate_charts/day14_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLastCart(t *testing.T) {
+
+}
