9a79dc1eea7da90c3f2ac81b96542b9d50281a98
Björn Gylling <bjorn.gylling@gmail.com>
2018-12-16 22:00:06 +0100

    add solution for day 14 part 1

--- 14_chocolate_charts/day14.go
+++ 14_chocolate_charts/day14.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+func arrayToString(l []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(l), " ", delim, -1), "[]")
+}
+
+func scores(iterations int) []int {
+	scores := []int{3, 7}
+	e1, e2 := 0, 1
+	for i := 0; i < iterations+10; i++ {
+		sum := scores[e1] + scores[e2]
+		if sum >= 10 {
+			scores = append(scores, sum/10, sum%10)
+		} else {
+			scores = append(scores, sum)
+		}
+		e1 = (e1 + 1 + scores[e1]) % len(scores)
+		e2 = (e2 + 1 + scores[e2]) % len(scores)
+	}
+	return scores[iterations : iterations+10]
+}
+
 func main() {
-	fmt.Printf("Day 14 part 1 result: %+v\n", nil)
+	fmt.Printf("Day 14 part 1 result: %+v\n", arrayToString(scores(236021), ""))
 
 	fmt.Printf("Day 14 part 2 result: %+v\n", nil)
 }
--- 14_chocolate_charts/day14_test.go
+++ 14_chocolate_charts/day14_test.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestLastCart(t *testing.T) {
-
+func TestScores(t *testing.T) {
+	r := scores(5)
+	e := []int{0, 1, 2, 4, 5, 1, 5, 8, 9, 1}
+	if !reflect.DeepEqual(r, e) {
+		t.Errorf("Expected the ten scores after 5 iterations to be %+v but was %+v", e, r)
+	}
+	r = scores(18)
+	e = []int{9, 2, 5, 1, 0, 7, 1, 0, 8, 5}
+	if !reflect.DeepEqual(r, e) {
+		t.Errorf("Expected the ten scores after 5 iterations to be %+v but was %+v", e, r)
+	}
+	r = scores(2018)
+	e = []int{5, 9, 4, 1, 4, 2, 9, 8, 8, 2}
+	if !reflect.DeepEqual(r, e) {
+		t.Errorf("Expected the ten scores after 5 iterations to be %+v but was %+v", e, r)
+	}
 }
