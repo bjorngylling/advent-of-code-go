dfc04f194b26a7c511dfb1a0ef5d17827fa1e455
Björn Gylling <bjorn.gylling@gmail.com>
2018-12-20 11:06:41 +0100

    add input parsing for day 15

--- 15_beverage_bandits/day15.go
+++ 15_beverage_bandits/day15.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"image"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 type EntityType = int
@@ -29,6 +32,26 @@ func NewEntity(t EntityType, p Position) *Entity {
 	return &Entity{Type: t, Pos: p, HP: 200, AP: 3}
 }
 
+type Entities []*Entity
+
+func (entities Entities) Filter(f func(*Entity) bool) (r Entities) {
+	for _, e := range entities {
+		if f(e) {
+			r = append(r, e)
+		}
+	}
+	return
+}
+
+func (entities Entities) Any(f func(*Entity) bool) bool {
+	for _, e := range entities {
+		if f(e) {
+			return true
+		}
+	}
+	return false
+}
+
 type Cave struct {
 	m      []bool
 	Width  int
@@ -43,8 +66,39 @@ func (c *Cave) Blocked(x, y int) bool {
 	return c.m[x+y*c.Width]
 }
 
+func (c *Cave) SetBlocked(x, y int, val bool) {
+	c.m[x+y*c.Width] = val
+}
+
+func parseInput(in string) (*Cave, Entities) {
+	lines := strings.Split(in, "\n")
+	cave := NewCave(len(lines[0]), len(lines))
+	var entities Entities
+
+	for y, ln := range lines {
+		for x, c := range ln {
+			switch c {
+			case '#':
+				cave.SetBlocked(x, y, true)
+			case 'G':
+				entities = append(entities, NewEntity(GOBLIN, Pos(x, y)))
+			case 'E':
+				entities = append(entities, NewEntity(ELF, Pos(x, y)))
+			}
+		}
+	}
+
+	return cave, entities
+}
+
 func main() {
-	fmt.Printf("Day 15 part 1 result: %+v\n", nil)
+	fileContent, err := ioutil.ReadFile("15_beverage_bandits/day15_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cave, entities := parseInput(string(fileContent))
+	fmt.Printf("Day 15 part 1 result: %+v, %+v\n", len(entities), cave.Blocked(0, 0))
 
 	fmt.Printf("Day 15 part 2 result: %+v\n", nil)
 }
--- 15_beverage_bandits/day15_test.go
+++ 15_beverage_bandits/day15_test.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestRounds(t *testing.T) {
-	r := 0
-	e := 5
-	if !reflect.DeepEqual(r, e) {
-		t.Errorf("Expected %+v but was %+v", e, r)
+var data = `#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######`
+
+func TestParseInput(t *testing.T) {
+	rCave, rEntities := parseInput(data)
+	if rCave.Width != 7 {
+		t.Errorf("Expected cave width to be 7 but was %+v", rCave.Width)
+	}
+	if rCave.Height != 7 {
+		t.Errorf("Expected cave height to be 7 but was %+v", rCave.Height)
+	}
+	if len(rEntities) != 6 {
+		t.Errorf("Expected entity count to be 6 but was %+v", len(rEntities))
+	}
+	if len(rEntities.Filter(func(e *Entity) bool { return e.Type == GOBLIN })) != 4 {
+		t.Errorf("Expected entity count to be 6 but was %+v", len(rEntities))
+	}
+	if len(rEntities.Filter(func(e *Entity) bool { return e.Type == ELF })) != 2 {
+		t.Errorf("Expected entity count to be 6 but was %+v", len(rEntities))
 	}
 }
