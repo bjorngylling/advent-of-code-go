19b93b8bc3da3144ac27954d42b1855f2073938c
Björn Gylling <bjorn.gylling@gmail.com>
2019-11-09 11:50:32 +0100

    Add template

---
+++ 00_template/input.go
@@ -0,0 +1,17 @@
+package main
+
+type Tests []struct {
+	Puzzle    string
+	Solution1 string
+	Solution2 string
+}
+
+var testPuzzles = Tests{
+	{
+		Puzzle:    "",
+		Solution1: "",
+		Solution2: "",
+	},
+}
+
+var puzzle = ``
