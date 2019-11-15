aa155f3b0d8a4684db2375bc517321978c6b70fc
Björn Gylling <bjorn.gylling@gmail.com>
2019-11-15 20:16:24 +0100

    Skip test assert if expected solution is empty string

--- 2016/01_no_time_for_a_taxicab/main_test.go
+++ 2016/01_no_time_for_a_taxicab/main_test.go
@@ -9,10 +9,10 @@ func Test_solve(t *testing.T) {
 	for i, tt := range testPuzzles {
 		t.Run(fmt.Sprint("example ", i), func(t *testing.T) {
 			part1, part2 := solve(tt.Puzzle)
-			if part1 != tt.Solution1 {
+			if tt.Solution1 != "" && part1 != tt.Solution1 {
 				t.Errorf("solve() part1 = %v, want %v", part1, tt.Solution1)
 			}
-			if part2 != tt.Solution2 {
+			if tt.Solution2 != "" && part2 != tt.Solution2 {
 				t.Errorf("solve() part2 = %v, want %v", part2, tt.Solution2)
 			}
 		})
