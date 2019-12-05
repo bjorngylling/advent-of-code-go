cbe458e62bcf78557960e8041ee2b0c7b24fd4e7
Björn Gylling <bjorn.gylling@gmail.com>
2019-12-05 07:30:10 +0100

    Copy computer from day 2 and remove day 2 specifics

---
+++ 2019/05_sunny_with_a_chance_of_asteroids/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type instruction = int
+
+const (
+	ADD = 1
+	MUL = 2
+
+	EXIT = 99
+)
+
+type memory []instruction
+
+func solve(input string) (string, string) {
+	var reg memory
+	for _, instr := range strings.Split(input, ",") {
+		if i, e := strconv.Atoi(instr); e == nil {
+			reg = append(reg, i)
+		}
+	}
+	if reg == nil {
+		panic(fmt.Errorf("reg is empty, unable parse to any instructions from input"))
+	}
+
+	return "", ""
+}
+
+func run(reg memory) memory {
+	for instrPntr := 0; reg[instrPntr] != EXIT; instrPntr += 4 {
+		op := reg[instrPntr]
+		p1Addr := reg[instrPntr+1]
+		p2Addr := reg[instrPntr+2]
+		tarAddr := reg[instrPntr+3]
+		switch op {
+		case ADD:
+			reg[tarAddr] = reg[p1Addr] + reg[p2Addr]
+		case MUL:
+			reg[tarAddr] = reg[p1Addr] * reg[p2Addr]
+		}
+	}
+	return reg
+}
+
+func (r memory) String() string {
+	var strSlice []string
+	for _, i := range r {
+		strSlice = append(strSlice, strconv.Itoa(i))
+	}
+	return strings.Join(strSlice, ",")
+}
+
+func main() {
+	start := time.Now()
+	part1, part2 := solve(puzzle)
+	elapsed := time.Since(start)
+
+	fmt.Printf("Part 1: %s\nPart 2: %s\n", part1, part2)
+	fmt.Printf("Program took %s", elapsed)
+}
