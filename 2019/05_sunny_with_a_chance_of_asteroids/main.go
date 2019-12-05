ee985e99e536e58bb593dcc8b5d4d7a97be2a7cf
Björn Gylling <bjorn.gylling@gmail.com>
2019-12-05 16:25:43 +0100

    Add input and output operations to the ship computer, WIP

--- 2019/05_sunny_with_a_chance_of_asteroids/main.go
+++ 2019/05_sunny_with_a_chance_of_asteroids/main.go
@@ -8,10 +8,13 @@ import (
 )
 
 type instruction = int
+type paramMode = int
 
 const (
 	ADD = 1
 	MUL = 2
+	IN  = 3
+	OUT = 4
 
 	EXIT = 99
 )
@@ -33,19 +36,38 @@ func solve(input string) (string, string) {
 }
 
 func run(reg memory) memory {
-	for instrPntr := 0; reg[instrPntr] != EXIT; instrPntr += 4 {
-		op := reg[instrPntr]
-		p1Addr := reg[instrPntr+1]
-		p2Addr := reg[instrPntr+2]
-		tarAddr := reg[instrPntr+3]
+	for instrPntr := 0; reg[instrPntr] != EXIT; {
+		op, p1Mode, p2Mode, tarMode := parseOp(reg[instrPntr])
 		switch op {
 		case ADD:
-			reg[tarAddr] = reg[p1Addr] + reg[p2Addr]
+			*param(reg, instrPntr+3, tarMode) = *param(reg, instrPntr+1, p1Mode) + *param(reg, instrPntr+2, p2Mode)
+			instrPntr += 4
 		case MUL:
-			reg[tarAddr] = reg[p1Addr] * reg[p2Addr]
+			*param(reg, instrPntr+3, tarMode) = *param(reg, instrPntr+1, p1Mode) * *param(reg, instrPntr+2, p2Mode)
+			instrPntr += 4
+		case IN:
+			fmt.Scanf("%d", *param(reg, instrPntr+1, tarMode))
+			instrPntr += 2
+		case OUT:
+			fmt.Printf("ptr=%d, output=%d\n", instrPntr, *param(reg, instrPntr+1, tarMode))
+			instrPntr += 2
 		}
 	}
 	return reg
+}
+
+func param(reg memory, pntr int, mode paramMode) *int {
+	switch mode {
+	case 0:
+		return &reg[reg[pntr]]
+	case 1:
+		return &reg[pntr]
+	}
+	return nil
+}
+
+func parseOp(instr int) (instruction, paramMode, paramMode, paramMode) {
+	return instr % 100, instr / 100 % 10, instr / 1000 % 10, instr / 10000 % 10
 }
 
 func (r memory) String() string {
@@ -57,6 +79,8 @@ func (r memory) String() string {
 }
 
 func main() {
+	run(memory{3, 0, 4, 0, 99})
+
 	start := time.Now()
 	part1, part2 := solve(puzzle)
 	elapsed := time.Since(start)
--- 2019/05_sunny_with_a_chance_of_asteroids/main_test.go
+++ 2019/05_sunny_with_a_chance_of_asteroids/main_test.go
@@ -18,11 +18,45 @@ func Test_run(t *testing.T) {
 		{name: "example_02", args: args{reg: memory{2, 3, 0, 3, 99}}, want: memory{2, 3, 0, 6, 99}},
 		{name: "example_03", args: args{reg: memory{2, 4, 4, 5, 99, 0}}, want: memory{2, 4, 4, 5, 99, 9801}},
 		{name: "example_04", args: args{reg: memory{1, 1, 1, 4, 99, 5, 6, 0, 99}}, want: memory{30, 1, 1, 4, 2, 5, 6, 0, 99}},
+		{name: "example_param_mode", args: args{reg: memory{1002, 4, 3, 4, 33}}, want: memory{1002, 4, 3, 4, 99}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := run(tt.args.reg); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("run() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseOp(t *testing.T) {
+	type args struct {
+		instr int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  instruction
+		want1 int
+		want2 int
+		want3 int
+	}{
+		{name: "", args: args{instr: 1002}, want: 2, want1: 0, want2: 1, want3: 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, got2, got3 := parseOp(tt.args.instr)
+			if got != tt.want {
+				t.Errorf("parseOp() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("parseOp() got1 = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("parseOp() got2 = %v, want %v", got2, tt.want2)
+			}
+			if got3 != tt.want3 {
+				t.Errorf("parseOp() got3 = %v, want %v", got3, tt.want3)
 			}
 		})
 	}
