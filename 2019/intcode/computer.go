a776422f884e0558c2e54db7727e9ed9084ab183
Björn Gylling <bjorn.gylling@gmail.com>
2019-12-09 14:29:03 +0100

    Refactor IntCode computer

--- 2019/intcode/computer.go
+++ 2019/intcode/computer.go
@@ -35,9 +35,10 @@ const (
 )
 
 type Computer struct {
-	Mem Memory
-	In  chan int
-	Out chan int
+	Mem     Memory
+	relBase int
+	In      chan int
+	Out     chan int
 }
 
 func Init(program string, in chan int, out chan int) (Computer, error) {
@@ -55,71 +56,79 @@ func parseInstr(instr int) (operation, paramMode, paramMode, paramMode) {
 	return instr % 100, instr / 100 % 10, instr / 1000 % 10, instr / 10000 % 10
 }
 
-func (c *Computer) expandIfReq(ptr int) {
-	if len(c.Mem) < ptr {
-		c.Mem = append(c.Mem, make(Memory, ptr-len(c.Mem)+50)...)
+func (c *Computer) readMem(ptr int, mode paramMode) int {
+	addr := c.getAddr(mode, ptr)
+	if len(c.Mem) > addr && addr >= 0 {
+		return c.Mem[addr]
+	} else {
+		return 0
 	}
 }
 
-func (c *Computer) Run() {
-	relBase := 0
-	param := func(ptr int, mode paramMode) *int {
-		switch mode {
-		case modePosition:
-			c.expandIfReq(c.Mem[ptr])
-			return &c.Mem[c.Mem[ptr]]
-		case modeImmediate:
-			c.expandIfReq(ptr)
-			return &c.Mem[ptr]
-		case modeRelativeBase:
-			c.expandIfReq(relBase + c.Mem[ptr])
-			return &c.Mem[relBase+c.Mem[ptr]]
-		}
-		return nil
+func (c *Computer) writeMem(ptr int, mode paramMode, value int) {
+	addr := c.getAddr(mode, ptr)
+	if len(c.Mem) < addr {
+		c.Mem = append(c.Mem, make(Memory, addr-len(c.Mem)+50)...)
 	}
+	c.Mem[addr] = value
+}
+
+func (c *Computer) getAddr(mode paramMode, ptr int) int {
+	switch mode {
+	case modePosition:
+		return c.Mem[ptr]
+	case modeImmediate:
+		return ptr
+	case modeRelativeBase:
+		return c.relBase + c.Mem[ptr]
+	}
+	return 0
+}
+
+func (c *Computer) Run() {
 	for instrPtr := 0; c.Mem[instrPtr] != exit; {
 		op, p1Mode, p2Mode, tarMode := parseInstr(c.Mem[instrPtr])
 		switch op {
 		case add:
-			*param(instrPtr+3, tarMode) = *param(instrPtr+1, p1Mode) + *param(instrPtr+2, p2Mode)
+			c.writeMem(instrPtr+3, tarMode, c.readMem(instrPtr+1, p1Mode)+c.readMem(instrPtr+2, p2Mode))
 			instrPtr += 4
 		case mul:
-			*param(instrPtr+3, tarMode) = *param(instrPtr+1, p1Mode) * *param(instrPtr+2, p2Mode)
+			c.writeMem(instrPtr+3, tarMode, c.readMem(instrPtr+1, p1Mode)*c.readMem(instrPtr+2, p2Mode))
 			instrPtr += 4
 		case in:
-			*param(instrPtr+1, p1Mode) = <-c.In
+			c.writeMem(instrPtr+1, p1Mode, <-c.In)
 			instrPtr += 2
 		case out:
-			c.Out <- *param(instrPtr+1, p1Mode)
+			c.Out <- c.readMem(instrPtr+1, p1Mode)
 			instrPtr += 2
 		case jt:
-			if *param(instrPtr+1, p1Mode) != 0 {
-				instrPtr = *param(instrPtr+2, p2Mode)
+			if c.readMem(instrPtr+1, p1Mode) != 0 {
+				instrPtr = c.readMem(instrPtr+2, p2Mode)
 			} else {
 				instrPtr += 3
 			}
 		case jf:
-			if *param(instrPtr+1, p1Mode) == 0 {
-				instrPtr = *param(instrPtr+2, p2Mode)
+			if c.readMem(instrPtr+1, p1Mode) == 0 {
+				instrPtr = c.readMem(instrPtr+2, p2Mode)
 			} else {
 				instrPtr += 3
 			}
 		case lt:
-			if *param(instrPtr+1, p1Mode) < *param(instrPtr+2, p2Mode) {
-				*param(instrPtr+3, tarMode) = 1
+			if c.readMem(instrPtr+1, p1Mode) < c.readMem(instrPtr+2, p2Mode) {
+				c.writeMem(instrPtr+3, tarMode, 1)
 			} else {
-				*param(instrPtr+3, tarMode) = 0
+				c.writeMem(instrPtr+3, tarMode, 0)
 			}
 			instrPtr += 4
 		case eq:
-			if *param(instrPtr+1, p1Mode) == *param(instrPtr+2, p2Mode) {
-				*param(instrPtr+3, tarMode) = 1
+			if c.readMem(instrPtr+1, p1Mode) == c.readMem(instrPtr+2, p2Mode) {
+				c.writeMem(instrPtr+3, tarMode, 1)
 			} else {
-				*param(instrPtr+3, tarMode) = 0
+				c.writeMem(instrPtr+3, tarMode, 0)
 			}
 			instrPtr += 4
 		case arb:
-			relBase += *param(instrPtr+1, p1Mode)
+			c.relBase += c.readMem(instrPtr+1, p1Mode)
 			instrPtr += 2
 		default:
 			fmt.Printf("unknown operation: ptr=%d, op=%d, p1Mode=%d, p2Mode=%d, tarMode=%d\n", instrPtr, op, p1Mode, p2Mode, tarMode)
