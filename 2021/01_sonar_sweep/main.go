cac9a2ae55bead931dff7658091eb7378ab7f218
Björn Gylling <bjorn.gylling@gmail.com>
2021-12-01 07:38:12 +0100

    feat: add solution for 2021 day 1

---
+++ 2021/01_sonar_sweep/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bjorngylling/advent-of-code/util"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func part1(scan []int) int {
+	incr := 0
+	for i, ln := range scan {
+		if i > 0 && ln > scan[i-1] {
+			incr++
+		}
+	}
+	return incr
+}
+
+func sum(lst []int) int {
+	sum := 0
+	for _, ln := range lst {
+		sum += ln
+	}
+	return sum
+}
+
+func part2(scan []int) int {
+	incr := 0
+	for i := 0; i <= len(scan); i++ {
+		if i > 3 && sum(scan[i-3:i]) > sum(scan[i-4:i-1]) {
+			incr++
+		}
+	}
+	return incr
+}
+
+func solve(input string) (string, string) {
+	lns := strings.Split(input, "\n")
+	var scan []int
+	for _, ln := range lns {
+		scan = append(scan, util.GetInt(ln))
+	}
+
+	return strconv.Itoa(part1(scan)), strconv.Itoa(part2(scan))
+}
+
+func main() {
+	start := time.Now()
+	part1, part2 := solve(puzzle)
+	elapsed := time.Since(start)
+
+	fmt.Printf("Part 1: %s\nPart 2: %s\n", part1, part2)
+	fmt.Printf("Program took %s", elapsed)
+}
